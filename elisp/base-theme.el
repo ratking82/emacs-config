bfb5c9d04e6d1068fec6950bc71266e5c9ef62cf
Samrat Roy <samrat.roy@kuleuven.be>
2018-05-18 08:34:17 +0200

    Change default theme to solarized-dark
    Moved to solarized dark. I like it better

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -1,7 +1,13 @@
 (use-package zenburn-theme
   :defer t
+  :disabled t
   :init
   (load-theme 'zenburn t))
+
+(use-package solarized-theme
+  :defer t
+  :init
+  (load-theme 'solarized-dark t))
 
 (use-package leuven-theme
   :defer t)
