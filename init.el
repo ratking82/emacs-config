f27e709dd7241ddb80ed60da28c2acdf957a0804
Samrat Roy <samratroy@gmail.com>
2019-01-19 18:37:54 +0100

    Remove unnecessary package initialise

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+;(package-initialize)
 
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
 ;;(addpf-to-list 'load-path "~/local/share/emacs/site-lisp/rtags")
